536c387c7a6e6b3c656a55c5cefe713975971c2e
scarlett <xiaoran7q@gmail.com>
2024-09-11 21:19:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function show() {
+ 
+    document.getElementById('content')
+            .style.display = "block";
+
+    document.getElementById('btnID')
+            .style.display = "none";
+}
+
+
+
+
